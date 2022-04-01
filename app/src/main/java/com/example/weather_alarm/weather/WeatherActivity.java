7659863e3eac3d892e2c3292eb3f413f529e2284
choihj619 <hjchoi619@gmail.com>
2022-04-01 13:31:52 +0900

    first commit

---
+++ app/src/main/java/com/example/weather_alarm/weather/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.example.weather_alarm.weather;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.weather_alarm.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
